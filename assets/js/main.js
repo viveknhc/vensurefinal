a3012411108c2e4bf6e195a5b2587b48911d10f8
viveknhc <viveknhc@gmail.com>
2022-12-11 04:49:42 -0800

    eve

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-(function($) {
-    'use strict';
-    $(function() {
-      $('[data-toggle="offcanvas"]').on("click", function() {
-        $('.sidebar-offcanvas').toggleClass('active')
-      });
+(function ($) {
+  "use strict";
+  $(function () {
+    $('[data-toggle="offcanvas"]').on("click", function () {
+      $(".sidebar-offcanvas").toggleClass("active");
     });
-  })(jQuery);
+  });
+})(jQuery);
 
+
+
+// clipboard
+function clipboard(elem, event) {
+  elem.prev('input[type="text"]').focus().select();
+  document.execCommand(event);
+  elem.prev('input[type="text"]').blur();
+  elem.addClass("clicked");
+  setTimeout(function () {
+    elem.removeClass("clicked");
+  }, 500);
+}
+
+$(".btn-copy").on("click", function () {
+  clipboard($(this), "copy");
+});
+
+$(".btn-cut").on("click", function () {
+  clipboard($(this), "cut");
+});
